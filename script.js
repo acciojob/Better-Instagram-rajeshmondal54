d1e6ce1dfdf32010ddc8832a167d60e11093ce69
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 01:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-const draggables = document.querySelectorAll('.image');
-let draggedElement = null;
+//your code here
+document.addEventListener('DOMContentLoaded', (event) => {
+    const images = document.querySelectorAll('.image');
 
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', (e) => {
-    draggedElement = e.target;
-    e.dataTransfer.setData('text/plain', null); // for Firefox compatibility
-  });
+    let dragSrcEl = null;
 
-  draggable.addEventListener('dragover', (e) => {
-    e.preventDefault();
-  });
+    function handleDragStart(e) {
+        dragSrcEl = this;
 
-  draggable.addEventListener('drop', (e) => {
-    e.preventDefault();
-    if (draggedElement !== e.target) {
-      // Swap the background images
-      let draggedElementBG = draggedElement.style.backgroundImage;
-      draggedElement.style.backgroundImage = e.target.style.backgroundImage;
-      e.target.style.backgroundImage = draggedElementBG;
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
     }
-  });
+
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
+
+        if (dragSrcEl != this) {
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        images.forEach(function (image) {
+            image.classList.remove('over');
+        });
+    }
+
+    images.forEach(function (image) {
+        image.addEventListener('dragstart', handleDragStart, false);
+        image.addEventListener('dragenter', handleDragEnter, false);
+        image.addEventListener('dragover', handleDragOver, false);
+        image.addEventListener('dragleave', handleDragLeave, false);
+        image.addEventListener('drop', handleDrop, false);
+        image.addEventListener('dragend', handleDragEnd, false);
+    });
 });
