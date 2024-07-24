01cc5c11aa8404309e017baaa1a8fbbebe9d56e3
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 01:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your code here
 const draggables = document.querySelectorAll('.image');
 let draggedElement = null;
 
 draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', (e) => {
     draggedElement = e.target;
-    e.dataTransfer.setData('text/plain', null);
+    e.dataTransfer.setData('text/plain', null); // for Firefox compatibility
   });
 
   draggable.addEventListener('dragover', (e) => {
@@ -15,6 +14,7 @@ draggables.forEach(draggable => {
   draggable.addEventListener('drop', (e) => {
     e.preventDefault();
     if (draggedElement !== e.target) {
+      // Swap the background images
       let draggedElementBG = draggedElement.style.backgroundImage;
       draggedElement.style.backgroundImage = e.target.style.backgroundImage;
       e.target.style.backgroundImage = draggedElementBG;
