ba7e81b154641a2e45b2d8345f7af2f1dee6f567
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 01:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+const draggables = document.querySelectorAll('.image');
+let draggedElement = null;
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', (e) => {
+    draggedElement = e.target;
+    e.dataTransfer.setData('text/plain', null);
+  });
+
+  draggable.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  draggable.addEventListener('drop', (e) => {
+    e.preventDefault();
+    if (draggedElement !== e.target) {
+      let draggedElementBG = draggedElement.style.backgroundImage;
+      draggedElement.style.backgroundImage = e.target.style.backgroundImage;
+      e.target.style.backgroundImage = draggedElementBG;
+    }
+  });
+});
